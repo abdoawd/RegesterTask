e2ca80cf20b03b0961710d63e48a7fcd007d7a18
abdulrahman <awadabdo222@gmail.com>
2018-07-18 13:00:03 +0200

    add parcelable to model and city and clear refferences

--- app/src/main/java/com/example/abdulrahman/regestertask/beans/City.java
+++ app/src/main/java/com/example/abdulrahman/regestertask/beans/City.java
@@ -1,21 +1,32 @@
 package com.example.abdulrahman.regestertask.beans;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
 /**
  * Created by abdulrahman on 7/16/2018.
  */
 
-public class City {
+public class City implements Parcelable {
+    public static final Parcelable.Creator<City> CREATOR = new Parcelable.Creator<City>() {
+        @Override
+        public City createFromParcel(Parcel source) {
+            return new City(source);
+        }
+
+        @Override
+        public City[] newArray(int size) {
+            return new City[size];
+        }
+    };
     @SerializedName("countryId")
     private String countryId;
-
     @SerializedName("titleAR")
     private String titleAR;
-
     @SerializedName("titleEN")
     private String titleEN;
-
     @SerializedName("id")
     private String id;
 
@@ -24,6 +35,13 @@ public class City {
         this.titleAR = titleAR;
         this.titleEN = titleEN;
         this.id = id;
+    }
+
+    protected City(Parcel in) {
+        this.countryId = in.readString();
+        this.titleAR = in.readString();
+        this.titleEN = in.readString();
+        this.id = in.readString();
     }
 
     public String getCountryId() {
@@ -59,5 +77,16 @@ public class City {
         this.id = id;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.countryId);
+        dest.writeString(this.titleAR);
+        dest.writeString(this.titleEN);
+        dest.writeString(this.id);
+    }
 }
